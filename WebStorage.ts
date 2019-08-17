a5f00a21dbb56955562276bc5c3ca623ccb7f9b6
krsln <krsln@users.noreply.github.com>
2019-08-17 17:46:00 +0300

    Update WebStorage.ts

--- WebStorage.ts
+++ WebStorage.ts
@@ -1,17 +1,10 @@
-// HTML Web Storage Objects
-export enum StorageType {
-  Local,
-  Session,
-  Cookie
-}
-
 export class WebStorage {
   /*
    window.localStorage - stores data with no expiration date
    window.sessionStorage - stores data for one session (data is lost when the browser tab is closed)
-
    sessionStorage.clear(); // Remove all saved data from sessionStorage
    */
+
   static Set(storageType: StorageType, key: string, obj: any, expMin: number = 60): any {
     // const expires = new Date(year, month, day, hours, minutes, seconds, milliseconds);
     const expires = new Date();
@@ -26,8 +19,11 @@ export class WebStorage {
     } else if (storageType === StorageType.Session) {
       sessionStorage.setItem(key, JSON.stringify(data));
     } else if (storageType === StorageType.Cookie) {
-      CookieMonster.Set(key, data, '/');
+      const path = '/';
+      const expireCookie: string = 'expires=' + new Date(data.ExpiresAt).toUTCString();
+      document.cookie = key + '=' + JSON.stringify(data) + '; ' + expireCookie + (path.length > 0 ? '; path=' + path : '');
     }
+
     return this.Get(storageType, key);
   }
 
@@ -38,7 +34,18 @@ export class WebStorage {
     } else if (storageType === StorageType.Session) {
       data = sessionStorage.getItem(key);
     } else if (storageType === StorageType.Cookie) {
-      data = CookieMonster.Get(key);
+      const cookies: Array<string> = document.cookie.split(';');
+      const cookieName = `${key}=`;
+
+      data = cookies
+        .map(x => x.replace(/^\s+/g, ''))
+        .find(x => x.indexOf(cookieName) === 0);
+
+      if (data) {
+        data = data.substring(cookieName.length, data.length);
+      } else {
+        data = null;
+      }
     }
 
     if (data !== 'undefined' && data !== undefined && data !== null) {
@@ -55,9 +62,17 @@ export class WebStorage {
       sessionStorage.removeItem(key);
       console.log(key, 'session expired');
     } else if (storageType === StorageType.Cookie) {
-      CookieMonster.Remove(key);
+      this.SetCookie(key, '', -1);
       console.log(key, ' Cookie expired');
     }
+  }
+
+  private static SetCookie(name: string, value: string, expireDays: number, path: string = '') {
+    const d: Date = new Date();
+    d.setTime(d.getTime() + expireDays * 24 * 60 * 60 * 1000);
+    // console.log(d);
+    const expires: string = 'expires=' + d.toUTCString();
+    document.cookie = name + '=' + value + '; ' + expires + (path.length > 0 ? '; path=' + path : '');
   }
 
   private static CheckData(storageType: StorageType, key: string, obj: any) {
@@ -72,36 +87,9 @@ export class WebStorage {
   }
 }
 
-export class CookieMonster {
-  static Get(name: string) {
-    const cookies: Array<string> = document.cookie.split(';');
-    const cookieName = `${name}=`;
-
-    const data = cookies
-      .map(x => x.replace(/^\s+/g, ''))
-      .find(x => x.indexOf(cookieName) === 0);
-
-    if (data) {
-      return data.substring(cookieName.length, data.length);
-    } else {
-      return null;
-    }
-  }
-
-  static Set(name: string, value: any, path: string = '') {
-    const expires: string = 'expires=' + new Date(value.ExpiresAt).toUTCString();
-    document.cookie = name + '=' + JSON.stringify(value) + '; ' + expires + (path.length > 0 ? '; path=' + path : '');
-  }
-
-  static Remove(name) {
-    this.SetCookie(name, '', -1);
-  }
-
-  static SetCookie(name: string, value: string, expireDays: number, path: string = '') {
-    const d: Date = new Date();
-    d.setTime(d.getTime() + expireDays * 24 * 60 * 60 * 1000);
-    // console.log(d);
-    const expires: string = 'expires=' + d.toUTCString();
-    document.cookie = name + '=' + value + '; ' + expires + (path.length > 0 ? '; path=' + path : '');
-  }
+export enum StorageType {
+  Local,
+  Session,
+  Cookie
 }
+
